7ee40e6806f8607401d408afef201941a5e5dac6
Gilsabo <gilsala_88@hotmail.com>
2023-09-22 20:16:39 +0200

    Add API and add state formSubmitted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [formSubmitted, setFormSubmitted] = useState('');
   const [guestList, setGuestList] = useState([]);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [id, setId] = useState(0);
-  console.log(guestList);
+  // const [id, setId] = useState(0);
+
+  const baseUrl = 'http://localhost:4000';
+
+  useEffect(() => {
+    const getGuests = async () => {
+      const response = await fetch(`${baseUrl}/guests`);
+      const allGuests = await response.json();
+      console.log(allGuests);
+      setGuestList(allGuests);
+    };
+    console.log(getGuests());
+  }, []);
+
+  useEffect(() => {
+    const getGuests = async () => {
+      const response = await fetch(`${baseUrl}/guests`);
+      const allGuests = await response.json();
+      console.log(allGuests);
+      setGuestList(allGuests);
+    };
+    console.log(getGuests());
+    setFormSubmitted(false);
+  }, [formSubmitted]);
 
   const handleRemove = (guestId) => {
     const guestListUpdated = guestList.filter((guest) => guest.id !== guestId);
@@ -15,7 +38,7 @@ export default function App() {
   return (
     <div className="App">
       <header>React guest list</header>
-      <form
+      <form /* ---------------------begining of the form----------- */
         onSubmit={(e) => {
           e.preventDefault();
         }}
@@ -36,20 +59,39 @@ export default function App() {
               value={lastName}
               onChange={(event) => setLastName(event.target.value)}
               onKeyDown={(e) => {
+                /* -------------------------------enter---------------*/
                 if (e.key === 'Enter') {
-                  setId(id + 1);
+                  /* setId(id + 1);
                   setGuestList([
                     ...guestList,
                     {
                       id: id,
-                      'First name': firstName,
-                      'Last name': lastName,
+                      FirstName: firstName,
+                      LastName: lastName,
                       attending: false,
                     },
-                  ]);
+                  ]); */
+
+                  setFormSubmitted(true);
                   setFirstName('');
                   setLastName('');
-                  console.log(id);
+
+                  // console.log(id);
+                  const post = async () => {
+                    const response = await fetch(`${baseUrl}/guests`, {
+                      method: 'POST',
+                      headers: {
+                        'Content-Type': 'application/json',
+                      },
+                      body: JSON.stringify({
+                        firstName: firstName,
+                        lastName: lastName,
+                      }),
+                    });
+                    const createdGuest = await response.json();
+                    console.log(createdGuest);
+                  };
+                  console.log(post());
                 }
               }}
             />
@@ -63,9 +105,8 @@ export default function App() {
             key={`div-name-${guest['First name']}${guest['Last name']}`}
             data-test-id="guest"
           >
-            <div>{guest['First name']}</div>
-            <div>{guest['Last name']}</div>
-
+            <div>{guest.firstName}</div>
+            <div>{guest.lastName}</div>
             <div>
               {guest.attending ? (
                 <div>Attending</div>
@@ -77,13 +118,29 @@ export default function App() {
                 id="attendingCheckbox"
                 aria-label="attending"
                 onChange={() => {
-                  guest.attending = !guest.attending;
-                  setGuestList([...guestList]);
+                  // guest.attending = !guest.attending;
+                  // setGuestList([...guestList]);
                 }}
               />
               <label htmlFor="attendingCheckbox">Attending to the event</label>
             </div>
-            <button type="button" onClick={() => handleRemove(guest.id)}>
+            <button
+              type="button"
+              onClick={() => {
+                handleRemove(guest.id);
+                const deleteGuest = async () => {
+                  const response = await fetch(
+                    `${baseUrl}/guests/${guest.id}`,
+                    {
+                      method: 'DELETE',
+                    },
+                  );
+                  const deletedGuest = await response.json();
+                  console.log(deletedGuest);
+                };
+                console.log(deleteGuest());
+              }}
+            >
               Remove
             </button>
           </div>
